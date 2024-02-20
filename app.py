9e162f8b1544e3f07e30baf4bba0d608dad5521d
Chow1essZzz <160450701+Chow1essZzz@users.noreply.github.com>
2024-02-20 15:36:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ app =Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-  return(render_template("index.html"))
+    return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  r=request.form.get("r")
-  return(render_template("main.html",r=r))
+    r = request.form.get("r")
+    return(render_template("main.html",r=r))
   
 @app.route("/imageGPT",methods=["GET","POST"])
 def main():
-  return(render_template("imageGPT.html"))
+    return(render_template("image_gpt.html"))
   
 if __name__=="__main__":
-  app.run()
+    app.run()
+
